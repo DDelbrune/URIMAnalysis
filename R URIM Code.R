8ef5ae4f87e1e7c91e227d8983ae4e21e443340f
Devante Delbrune <35347268+DDelbrune@users.noreply.github.com>
2023-09-17 13:57:22 -0400

    Add files via upload

---
+++ R URIM Code.R
@@ -0,0 +1,14 @@
+#Packages to Install
+install.packages("XLConnect")
+install.packages("tidyverse")
+install.packages("dplyr")
+install.packages("ggplot2")
+install.packages("htmlwidgets")
+
+
+#Run packages
+library("XLConnect")
+library("tidyverse")
+library("dplyr")
+library("ggplot2")
+library("htmlwidgets")
